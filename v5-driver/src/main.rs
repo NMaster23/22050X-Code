1dfab2b4b2dba7a6dbea308a5e7cfb8e911364b1
NMaster23 <nmmaster22@gmail.com>
2026-09-13 19:11:51 -0700

    Update main.rs

--- v5-driver/src/main.rs
+++ v5-driver/src/main.rs
@@ -46,7 +46,7 @@ fn cascade_control(controller: &ControllerState, mut cascade: &mut MotorGroup) {
     } else if controller.button_down.is_pressed() {
         let _ = cascade.set_velocity(-600);
     } else {
-        let _ = cascade.set_velocity(0);
+        let _ = cascade.brake(vexide::smart::motor::BrakeMode::Hold);
     }
 }
 
@@ -65,15 +65,14 @@ async fn main(peripherals: Peripherals) {
     let controller = peripherals.primary_controller;
     let mut intake = Motor::new(peripherals.port_19, Gearset::Blue, Direction::Forward);
     let mut cascade_left = Motor::new(peripherals.port_1, Gearset::Blue, Direction::Forward);
-    let mut cascade_right = Motor::new(peripherals.port_2, Gearset::Blue, Direction::Forward);
+    let mut cascade_right = Motor::new(peripherals.port_2, Gearset::Blue, Direction::Reverse);
     let mut left_front = Motor::new(peripherals.port_20, Gearset::Blue, Direction::Forward);
     let mut left_back = Motor::new(peripherals.port_8, Gearset::Blue, Direction::Reverse);
-    let mut right_front = Motor::new(peripherals.port_7, Gearset::Blue, Direction::Forward);
-    let mut right_back = Motor::new(peripherals.port_9, Gearset::Blue, Direction::Reverse);
+    let mut right_front = Motor::new(peripherals.port_7, Gearset::Blue, Direction::Reverse);
+    let mut right_back = Motor::new(peripherals.port_9, Gearset::Blue, Direction::Forward);
     let mut drivetrain_left = MotorGroup::new(vec![left_front, left_back]);
     let mut drivetrain_right = MotorGroup::new(vec![right_front, right_back]);
     let mut cascade = MotorGroup::new(vec![cascade_left, cascade_right]);
-    let _ = cascade.brake(vexide::smart::motor::BrakeMode::Hold);
     loop {
         let state = &controller.state().unwrap_or_default();
         drive(state, &mut drivetrain_left, &mut drivetrain_right);
